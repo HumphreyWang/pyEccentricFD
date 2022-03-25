fe39b55bd8f8f84f9a2f02fe10012ddf66634f28
HumphreyWang <wh7phy@qq.com>
2022-03-25 13:19:49 +0800

    prepare for new func

---
+++ InspiralEccentricFD.h
@@ -0,0 +1,70 @@
+//
+// Created by dice on 2022/3/23.
+//
+
+#ifndef _INSPIRALECCENTRICFD_H
+#define _INSPIRALECCENTRICFD_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <complex.h>
+#include <lal/Date.h>
+#include <lal/FrequencySeries.h>
+#include <lal/LALConstants.h>
+#include <lal/LALDatatypes.h>
+#include <lal/Units.h>
+#include <lal/XLALError.h>
+
+
+#include "gsl/gsl_matrix.h"
+#include "gsl/gsl_vector.h"
+#include "gsl/gsl_linalg.h"
+#include "gsl/gsl_eigen.h"
+#include "gsl/gsl_permutation.h"
+#include <gsl/gsl_errno.h>
+#include <gsl/gsl_math.h>
+#include <gsl/gsl_roots.h>
+#include <gsl/gsl_sf_bessel.h>
+#include <gsl/gsl_sf_gamma.h>
+#include <gsl/gsl_complex.h>
+#include <gsl/gsl_complex_math.h>
+#include <gsl/gsl_errno.h>
+#include <gsl/gsl_spline.h>
+#include <gsl/gsl_errno.h>
+#include <gsl/gsl_multiroots.h>
+#include <gsl/gsl_rng.h>
+
+#include "LALSimInspiralOptimizedCoefficientsEccentricityFD.c"
+
+#define gamma (0.577215664901532860606512090)
+
+int SimInspiralEccentricFD(COMPLEX16FrequencySeries **hptilde,
+                           COMPLEX16FrequencySeries **hctilde,
+                           REAL8 phiRef,
+                           REAL8 deltaF,
+                           REAL8 m1_SI,
+                           REAL8 m2_SI,
+                           REAL8 fStart,
+                           REAL8 fEnd,
+                           REAL8 i,
+                           REAL8 r,
+                           REAL8 inclination_azimuth,
+                           REAL8 e_min,
+                           int phaseO);
+
+int SimInspiralEccentricFDAmpPhase(COMPLEX16FrequencySeries **hptilde,
+                                   COMPLEX16FrequencySeries **hctilde,
+                                   REAL8 phiRef,
+                                   REAL8 deltaF,
+                                   REAL8 m1_SI,
+                                   REAL8 m2_SI,
+                                   REAL8 fStart,
+                                   REAL8 fEnd,
+                                   REAL8 i,
+                                   REAL8 r,
+                                   REAL8 inclination_azimuth,
+                                   REAL8 e_min,
+                                   int phaseO);
+
+#endif //_INSPIRALECCENTRICFD_H
--- main.c
+++ main.c
@@ -1,7 +1,31 @@
-#include <stdio.h>
+//#include <stdio.h>
+//#include <lal/LALDatatypes.h>
+
+#include "InspiralEccentricFD.h"
+
+int main(int argc, const char * argv[]) {
+    double m1 = strtod(argv[1], NULL);
+    double m2 = strtod(argv[2], NULL);
+    double e_min = strtod(argv[3], NULL);
+    double i = strtod(argv[4], NULL);
+    double inclination_azimuth = strtod(argv[5], NULL);
+    double phiRef = strtod(argv[6], NULL);
+    double Dl = strtod(argv[7], NULL);
+    double fStart = strtod(argv[8], NULL);
+    double fEnd = strtod(argv[9], NULL);
+    double deltaF = strtod(argv[10], NULL);
+
+    COMPLEX16FrequencySeries *hptilde=NULL;
+    COMPLEX16FrequencySeries *hctilde=NULL;
 
 
-int main() {
-    printf("Hello, World!\n");
+    SimInspiralEccentricFD(&hptilde, &hctilde, phiRef, deltaF, m1*LAL_MSUN_SI, m2*LAL_MSUN_SI,
+                           fStart, fEnd, i, Dl*1e6*LAL_PC_SI, inclination_azimuth, e_min, 7);
+
+
+    complex double x0 = (hptilde->data->data[3333]);
+    printf("%.15e+%.15ei", creal(x0), cimag(x0));
+
+
     return 0;
 }
